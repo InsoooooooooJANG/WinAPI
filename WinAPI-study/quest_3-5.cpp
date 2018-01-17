acaca87bf7c0969e965d083b8d5e886cb3fa0b72
insooooooooo <oasisholics@gmail.com>
2018-01-17 22:14:50 +0900

    click circle practice
    click the circle, It changes red color

---
+++ WinAPI-study/quest_3-5.cpp
@@ -0,0 +1,107 @@
+#include <windows.h>
+#include <tchar.h>
+#include <math.h>
+#define BSIZE 40
+
+double LengthPts(int x1, int y1, int x2, int y2)
+{
+  return (sqrt((float)((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1))));
+}
+
+BOOL InCircle(int x, int y, int mx, int my)
+{
+  if (LengthPts(x, y, mx, my) < BSIZE) return TRUE;
+  else return FALSE;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+  HWND hwnd;
+  MSG msg;
+  WNDCLASS WndClass;
+  WndClass.style = CS_HREDRAW | CS_VREDRAW;
+  WndClass.lpfnWndProc = WndProc;
+  WndClass.cbClsExtra = 0;
+  WndClass.cbWndExtra = 0;
+  WndClass.hInstance = hInstance;
+  WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+  WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+  WndClass.lpszMenuName = NULL;
+  WndClass.lpszClassName = _T("Window Class Name");
+  RegisterClass(&WndClass);
+
+  hwnd = CreateWindow(_T("Window Class Name"),
+    _T("Window Title Name"),
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    NULL,
+    NULL,
+    hInstance,
+    NULL);
+
+  ShowWindow(hwnd, nCmdShow);
+
+  UpdateWindow(hwnd);
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
+  return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+  HDC hdc;
+  PAINTSTRUCT ps;
+  static int x, y;
+  static BOOL Selection;
+  int mx, my;
+  HBRUSH hBrush, oldBrush;
+
+  switch (iMsg)
+  {
+  case WM_CREATE:
+    x = 20; y = 20;
+    break;
+  case WM_PAINT:
+    hdc = BeginPaint(hwnd, &ps);
+
+    if (Selection == TRUE)
+    {
+      hBrush = CreateSolidBrush(RGB(255, 0, 0));
+      oldBrush = (HBRUSH)SelectObject(hdc, hBrush);
+      Ellipse(hdc, x - 20, y - 20, x + 20, y + 20);
+      SelectObject(hdc, oldBrush);
+      DeleteObject(hBrush);
+    }
+    else
+    {
+      Ellipse(hdc, x - 20, y - 20, x + 20, y + 20);
+    }
+    EndPaint(hwnd, &ps);
+    break;
+  case WM_LBUTTONDOWN:
+    mx = LOWORD(lParam);
+    my = HIWORD(lParam);
+    if (InCircle(x, y, mx, my)) Selection = TRUE;
+    InvalidateRgn(hwnd, NULL, TRUE);
+    break;
+  case WM_LBUTTONUP:
+    Selection = FALSE;
+    InvalidateRgn(hwnd, NULL, TRUE);
+    break;
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  }
+
+  return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
